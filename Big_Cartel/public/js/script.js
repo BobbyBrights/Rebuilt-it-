ebbc95bd67e24a226dfb7cedbd05e449a6351c80
dlanorlecram <ronald.marcel.mr@gmail.com>
2017-10-26 11:34:48 +0200

    Refonte du script pour changer l'état de la navbar sous l'Event scroll.

--- Big_Cartel/public/js/script.js
+++ Big_Cartel/public/js/script.js
@@ -1,11 +1,30 @@
-$(document).ready(function(){
-  console.log('loaded!');
+'use strict';
+
+$(window).ready(function(){
+  /**
+  * Chargement de la vidéo
+  **/
   $('.container_intro').vide('asset/video/1.mp4');
-  $('.logo_nav').mouseover(function(){
-    $('.navbar').css("transform","translateY(0px)")
-  });
-  $('.logo_nav').mouseout(function(){
-    $('.navbar').css("transform","translateY(-70px)")
-  });
+
+  var $nav      = $('.navbar');
+  var previousPos = -1;
+
+  /**
+  * changement d'effet de la navbar sur l'event scroll
+  **/
+
+  $(window).on('scroll',function(){
+    var scrollPos = $(this).scrollTop();
+
+    if(scrollPos >= 70){
+      if(previousPos < scrollPos){
+        $nav.addClass('hidden');
+        previousPos = scrollPos;
+      }else{
+        $nav.removeClass('hidden');
+        previousPos = scrollPos;
+      }
+    }
+  })
 
 });
